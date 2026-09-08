55d07db9805e785c244fe6b28f7872c01b6b451c
YOUR_GITHUB_USERNAME <suryabhaskarv@curasoftware.com>
2026-09-08 16:18:08 +0530

    1 build

--- node-ci-cd/index.js
+++ node-ci-cd/index.js
@@ -18,18 +18,16 @@ app.get('/users', (req, res) => {
     return res.status(200).json({ users });
 })
 
+// app.get('/users/:id',(req,res)=>{
+//     const userId = parseInt(req.params.id);
 
-app.get('/users/:id',(req,res)=>{
-    const userId = parseInt(req.params.id);
-
-    const user = users.find(u => u.id === userId);
+//     const user = users.find(u => u.id === userId);
     
-    if(!user){
-        return res.status(404).json({ message: 'User not found'});
-    }
-    return res.status(200).json({ user });
-})
-
+//     if(!user){
+//         return res.status(404).json({ message: 'User not found'});
+//     }
+//     return res.status(200).json({ user });
+// })
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
