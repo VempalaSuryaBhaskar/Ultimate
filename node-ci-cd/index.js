4e4c4ac2c2a4b35e500ffb6acb9ae6fd720fd8b2
YOUR_GITHUB_USERNAME <suryabhaskarv@curasoftware.com>
2026-09-08 16:33:47 +0530

    2 build

--- node-ci-cd/index.js
+++ node-ci-cd/index.js
@@ -18,16 +18,18 @@ app.get('/users', (req, res) => {
     return res.status(200).json({ users });
 })
 
-app.get('/users/:id',(req,res)=>{
-    const userId = parseInt(req.params.id);
 
-    const user = users.find(u => u.id === userId);
+//commented the below code to test the CI/CD pipeline
+// app.get('/users/:id',(req,res)=>{
+//     const userId = parseInt(req.params.id);
+
+//     const user = users.find(u => u.id === userId);
     
-    if(!user){
-        return res.status(404).json({ message: 'User not found'});
-    }
-    return res.status(200).json({ user });
-})
+//     if(!user){
+//         return res.status(404).json({ message: 'User not found'});
+//     }
+//     return res.status(200).json({ user });
+// })
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
