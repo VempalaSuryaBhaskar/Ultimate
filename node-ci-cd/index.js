8205afca86d64cc92fc3e75d3de9c3c630fa756a
YOUR_GITHUB_USERNAME <suryabhaskarv@curasoftware.com>
2026-09-08 15:31:38 +0530

    second commit - 3

--- node-ci-cd/index.js
+++ node-ci-cd/index.js
@@ -21,7 +21,9 @@ app.get('/users', (req, res) => {
 
 app.get('/users/:id',(req,res)=>{
     const userId = parseInt(req.params.id);
+
     const user = users.find(u => u.id === userId);
+    
     if(!user){
         return res.status(404).json({ message: 'User not found'});
     }
