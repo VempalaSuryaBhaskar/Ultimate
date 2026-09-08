1ad4b828288f8016f238b3a8fbc8780307f05e2b
YOUR_GITHUB_USERNAME <suryabhaskarv@curasoftware.com>
2026-09-08 11:44:30 +0530

    new build version 2

--- node-ci-cd/index.js
+++ node-ci-cd/index.js
@@ -2,23 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    return res.status(200).json({message:'Hello World !'});
+const users = [
+    { id: 1, name: 'Jone Doe' },
+    { id: 2, name: 'Pavan Doe' },
+    { id: 3, name: 'Surya Doe' },
+    { id: 4, name: 'Ashish Doe' },
+    { id: 5, name: 'Karimnagar Doe' },
+]
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Hello World !' });
 })
 
-app.get('/users',(req,res)=>{
-    const users = [
-        {id:1,name:'Jone Doe'},
-        {id:2,name:'Pavan Doe'},
-        {id:3,name:'Surya Doe'},
-        {id:4,name:'Ashish Doe'},
-        {id:5,name:'Karimnagar Doe'},
-    ]
-
-    return res.status(200).json({users});
+app.get('/users', (req, res) => {
+    return res.status(200).json({ users });
 })
 
 
-app.listen(4000,()=>{
+app.get('/users/:id',(req,res)=>{
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+    if(!user){
+        return res.status(404).json({ message: 'User not found'});
+    }
+    return res.status(200).json({ user });
+})
+
+
+app.listen(4000, () => {
     console.log('Server is running on port 4000');
 })
